5ae4ce9d01e9ad2bce0384818d0123ebff0cefc4
haeyeoni <haeyeon8669@gmail.com>
2021-10-25 21:12:11 +0900

    changed to nodelet

--- publish_velodyne/src/main.cpp
+++ publish_velodyne/src/main.cpp
@@ -2,6 +2,9 @@
 #define WINVER 0x0A00
 #define _WIN32_WINNT 0x0A00
 
+#include <nodelet/nodelet.h>
+#include <pluginlib/class_list_macros.h>
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -22,6 +25,8 @@
 #include <pcl_conversions/pcl_conversions.h>
 
 // Point Type
+namespace publish_velodyne
+{
 
 typedef pcl::PointXYZI PointType;
 using std::cout; using std::endl;
@@ -31,90 +36,73 @@ using std::chrono::seconds;
 using std::chrono::system_clock;
 
 
-int main( int argc, char *argv[] )
+class PublishVelodyne : public nodelet::Nodelet
 {
-    ros::init(argc, argv, "publish_velodyne");
-    ROS_INFO("\033[1;32m---->\033[0m Publish Velodyne Started.");
-
-    if( pcl::console::find_switch( argc, argv, "-help" ) ){
-        std::cout << "usage: " << argv[0]
-            << " [-ipaddress <192.168.1.201>]"
-            << " [-port <2368>]"
-            << " [-pcap <*.pcap>]"
-            << " [-help]"
-            << std::endl;
-        return 0;
-    }
-
-    std::string ipaddress( "192.168.1.201" );
-    std::string port( "2369" );
-    std::string pcap;
-
-    pcl::console::parse_argument( argc, argv, "-ipaddress", ipaddress );
-    pcl::console::parse_argument( argc, argv, "-port", port );
-    pcl::console::parse_argument( argc, argv, "-pcap", pcap );
-
-    std::cout << "-ipadress : " << ipaddress << std::endl;
-    std::cout << "-port : " << port << std::endl;
-    std::cout << "-pcap : " << pcap << std::endl;
-
-    // Point Cloud
-    pcl::PointCloud<PointType>::ConstPtr cloud;
-
-    // Retrieved Point Cloud Callback Function
-    boost::mutex mutex;
-    auto millisec_prev = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-    auto millisec_curr = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-
-
-    boost::function<void( const pcl::PointCloud<PointType>::ConstPtr& )> function =
-        [ &cloud, &mutex ]( const pcl::PointCloud<PointType>::ConstPtr& ptr ){
-            boost::mutex::scoped_lock lock( mutex );
-            /* Point Cloud Processing */
-            cloud = ptr;
-        };
-
-    // VLP Grabber
-    boost::shared_ptr<pcl::VLPGrabber> grabber;
-    if( !pcap.empty() ){
-        std::cout << "Capture from PCAP..." << std::endl;
-        grabber = boost::shared_ptr<pcl::VLPGrabber>( new pcl::VLPGrabber( pcap ) );
-    }
-    else if( !ipaddress.empty() && !port.empty() ){
-        std::cout << "Capture from Sensor..." << std::endl;
-        grabber = boost::shared_ptr<pcl::VLPGrabber>( new pcl::VLPGrabber( boost::asio::ip::address::from_string( ipaddress ), boost::lexical_cast<unsigned short>( port ) ) );
-    }
-
-    // Register Callback Function
-    boost::signals2::connection connection = grabber->registerCallback( function );
-
-    // Start Grabber
-    grabber->start();
-
-    // ROS
+private:
     ros::NodeHandle nh;
-    ros::Publisher pub = nh.advertise<pcl::PointCloud<PointType>> ("velodyne_points", 1000);
-    pcl::PointCloud<PointType>::Ptr msg (new pcl::PointCloud<PointType>);
+    ros::Publisher pub;
+    pcl::PointCloud<PointType>::Ptr msg; 
 
-    while (nh.ok())
+    std::string ipaddress;
+    std::string port;
+    std::string pcap;
+    pcl::PointCloud<PointType>::ConstPtr cloud;
+    boost::shared_ptr<pcl::VLPGrabber> grabber;
+    boost::signals2::connection connection;
+    
+    boost::mutex mutex;
+    ros::Timer timer;
+
+public:
+    PublishVelodyne() = default;
+    virtual void onInit()
+    {
+   
+        ROS_INFO("\033[1;32m---->\033[0m Publish Velodyne Started.");
+        msg.reset(new pcl::PointCloud<PointType>());
+        pub = nh.advertise<pcl::PointCloud<PointType>> ("velodyne_points", 10, this);
+        boost::function<void(const pcl::PointCloud<PointType>::ConstPtr&)> cb = boost::bind(&PublishVelodyne::vlpCallback, this, _1);
+   
+        ipaddress = "192.168.1.201";
+        port = "2369";
+
+        grabber = boost::shared_ptr<pcl::VLPGrabber>(new pcl::VLPGrabber(boost::asio::ip::address::from_string(ipaddress), boost::lexical_cast<unsigned short>(port)));
+   
+        // Register Callback Function
+        connection = grabber->registerCallback(cb);
+
+        // Start Grabber
+        grabber->start();
+        timer = nh.createTimer(ros::Duration(0.005), boost::bind(&PublishVelodyne::publishTopic, this, _1));
+    }
+
+    //Callback
+    void vlpCallback(const pcl::PointCloud<PointType>::ConstPtr& cloudPtr)
+    {
+        // Lock and Copy
+        boost::mutex::scoped_lock lock(mutex);
+        cloud = cloudPtr;
+    }
+    void publishTopic(const ros::TimerEvent& event)
     {
         boost::mutex::scoped_lock lock( mutex );
         if( lock.owns_lock() && cloud ){
             pcl::copyPointCloud(*cloud, *msg);
             pcl_conversions::toPCL(ros::Time::now(), msg->header.stamp);
             msg->header.frame_id = "velodyne";
-            pub.publish (msg);
+            pub.publish(msg);
         }
-        ros::spinOnce ();
     }
+    ~PublishVelodyne()
+    {
+        // Stop Grabber
+        grabber->stop();
 
-    // Stop Grabber
-    grabber->stop();
-
-    // Disconnect Callback Function
-    if( connection.connected() ){
-        connection.disconnect();
+        // Disconnect Callback Function
+        if( connection.connected() ){
+            connection.disconnect();
+        }
     }
+};
 
-    return 0;
-}
+}PLUGINLIB_EXPORT_CLASS(publish_velodyne::PublishVelodyne, nodelet::Nodelet)
